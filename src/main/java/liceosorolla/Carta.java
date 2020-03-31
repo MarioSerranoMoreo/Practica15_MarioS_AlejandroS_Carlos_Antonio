a332b845213b5204fa49d6a114f3266272fae36a
Mario <Mario@DESKTOP-6U68MDR>
2020-03-31 17:51:08 +0200

    getters/setters etc carta

--- src/main/java/liceosorolla/Carta.java
+++ src/main/java/liceosorolla/Carta.java
@@ -3,4 +3,25 @@ package main.java.liceosorolla;
 public class Carta {
 	private boolean bocaArriba;
 	private Valor valor;
+	public Carta(boolean bocaArriba, Valor valor) {
+		super();
+		this.bocaArriba = bocaArriba;
+		this.valor = valor;
+	}
+	public boolean isBocaArriba() {
+		return bocaArriba;
+	}
+	public void setBocaArriba(boolean bocaArriba) {
+		this.bocaArriba = bocaArriba;
+	}
+	public Valor getValor() {
+		return valor;
+	}
+	public void setValor(Valor valor) {
+		this.valor = valor;
+	}
+	@Override
+	public String toString() {
+		return "Carta [bocaArriba=" + bocaArriba + ", valor=" + valor + "]";
+	}
 }
