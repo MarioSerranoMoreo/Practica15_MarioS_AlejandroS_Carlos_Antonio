cbc61b5db29da8be83ecf4887225d25d1e5271d7
Mario <Mario@DESKTOP-6U68MDR>
2020-03-31 18:17:17 +0200

    menu

--- src/main/java/liceosorolla/Start.java
+++ src/main/java/liceosorolla/Start.java
@@ -1,10 +1,44 @@
 package main.java.liceosorolla;
 
+import java.util.Scanner;
+
 public class Start {
 
 	public static void main(String[] args) {
 		Baraja baraja = new Baraja();
 		Tablero tablero = new Tablero();
+		int opcion=0;
+		Scanner teclado= new Scanner(System.in);
+		while(opcion!=3) {
+			String frase="¿Elige un numero segun lo que quieras hacer:\n1.Sacar nuevas cartas de la baraja\n2.Mover cartas de una columna a otra\n3.Acabar el juego";
+			opcion=compruebaInt(frase);
+			switch(opcion) {
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				 System.out.println("Adios");
+				break;
+			default:
+				System.out.println("Numero no valido");
+				break;
+			}
+		}
 	}
-
+	public static int compruebaInt(String frase) {
+		Scanner teclado= new Scanner(System.in);
+		int numero=0;
+		boolean bool=false;
+			while(!bool) {
+			System.out.println(frase);
+			bool=teclado.hasNextInt();
+			if(bool) {
+				numero=teclado.nextInt();
+			}
+		}
+		return numero;
+	}
 }
