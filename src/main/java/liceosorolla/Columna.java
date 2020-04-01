a3dbe135b79d3ce7584ced6705bf694bf0a39eab
alexs <alexs@LAPTOP-KQGHEVBO>
2020-04-01 21:07:17 +0200

    hola

--- src/main/java/liceosorolla/Columna.java
+++ src/main/java/liceosorolla/Columna.java
@@ -4,4 +4,38 @@ import java.util.ArrayList;
 
 public class Columna {
 	private ArrayList<Carta> cartas;
+	
+	public ArrayList<Carta> getCartas() {
+		return cartas;
+	}
+	public void setCartas(ArrayList<Carta> cartas) {
+		this.cartas = cartas;
+	}
+	
+	public Columna(ArrayList<Carta> cartas) {
+		this.cartas = cartas;
+	}
+	
+	public String toString() {
+		return "Carta"+"cartas";
+	}
+	
+	public boolean comprobarCarta(int cantidad) {
+		boolean devolver=true;
+		int numero=0;
+		int valor1;
+		int valor2;
+		
+		for(int i=cartas.size()-1; i>cartas.size()-cantidad||devolver; i--) {
+			valor1=cartas.get(i).getValor().ordinal();
+			valor2=cartas.get(i-1).getValor().ordinal();
+			if(valor1+1==valor2) {
+				devolver=true;
+			}else {
+				devolver=false;
+			}
+		}
+		
+		return devolver;
+	}
 }
