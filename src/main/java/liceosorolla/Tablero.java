f5ee246ff6a9ddc28b10730e36b2e31311911d7e
antonioojeda <antonioojeda@192.168.1.39>
2020-04-17 16:15:26 +0200

    H

--- src/main/java/liceosorolla/Tablero.java
+++ src/main/java/liceosorolla/Tablero.java
@@ -3,6 +3,13 @@ package main.java.liceosorolla;
 import java.util.ArrayList;
 
 public class Tablero {
+	
 	private ArrayList<Columna> columnas;
 	
+	public void meterCarta(int columna, boolean bool, Valor valor) {
+	  
+		columnas.get(columna).getCartas().add(new Carta(bool,valor));
+		
+	}
+
 }
